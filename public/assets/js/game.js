54967b44db35d2b78e362287888ca4ebf905413d
duclt3005 <duclt3005@gmail.com>
2023-10-13 11:12:37 +0700

    game full screen

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -2,29 +2,34 @@ const maxWidth = "width: 100%; height: auto;";
 const maxHeight = "height: 100%; width: auto;";
 
 function resizeGame() {
-  setCss('#unity-canvas');
-  setCss('canvas');
-  setCss('#screen');
-  setCss('#unity-container');
+    setCss('#unity-canvas');
+    setCss('canvas');
+    setCss('#screen');
+    setCss('#unity-container');
+    const unityContainer = document.querySelector("#unity-container");
+
+    if (unityContainer) {
+        unityContainer.style.cssText = "height:100%; width:100%;"
+    }
 }
 
 function setCss(cssName) {
-  const canvas = document.querySelector(cssName);
+    const canvas = document.querySelector(cssName);
 
-  if (!canvas) return;
+    if (!canvas) return;
 
-  const screenRatio = document.documentElement.clientWidth / document.documentElement.clientHeight;
-  const gameRatio = (canvas.offsetWidth ?? 1) / (canvas.offsetHeight ?? 1);
+    const screenRatio = document.documentElement.clientWidth / document.documentElement.clientHeight;
+    const gameRatio = (canvas.offsetWidth ?? 1) / (canvas.offsetHeight ?? 1);
 
-  canvas.style.cssText = screenRatio > gameRatio ? maxHeight : maxWidth;
+    canvas.style.cssText = screenRatio > gameRatio ? maxHeight : maxWidth;
 }
 
 window.onload = function () {
-  document.body.style.cssText =
-    "height: 100vh; overflow: hidden; margin: 0; display: flex; justify-content: center; align-items: center";
-  resizeGame();
+    document.body.style.cssText =
+        "height: 100vh; overflow: hidden; margin: 0; display: flex; justify-content: center; align-items: center";
+    resizeGame();
 };
 
 window.onresize = function () {
-  resizeGame();
+    resizeGame();
 };
